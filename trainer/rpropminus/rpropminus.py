71e202830b9d70417ade61e5aba9442096658a1d
giuliano <giuliano.aiello.97@gmail.com>
2025-03-29 11:01:54 +0100

    feat (rpropminus): add optimizer

--- trainer/rpropminus/rpropminus.py
+++ trainer/rpropminus/rpropminus.py
@@ -1,10 +1,10 @@
 import torch
 from torch.optim.optimizer import Optimizer
 
-class RpropPlus(Optimizer):
+class RpropMinus(Optimizer):
     def __init__(self, params, lr=0.012, etaminus=0.5, etaplus=1.2, delta_min=1e-6, delta_max=50):
         defaults = dict(lr=lr, etaminus=etaminus, etaplus=etaplus, delta_min=delta_min, delta_max=delta_max)
-        super(RpropPlus, self).__init__(params, defaults)
+        super(RpropMinus, self).__init__(params, defaults)
 
     @torch.no_grad()
     def step(self):
@@ -28,20 +28,17 @@ class RpropPlus(Optimizer):
 
                 step_size = state['step_size']
                 prev_grad = state['prev_grad']
-                prev_update = state['prev_update']
 
                 grad_sign = grad * prev_grad
 
                 increase_mask = grad_sign > 0
                 decrease_mask = grad_sign < 0
 
-                step_size[increase_mask] = torch.min(step_size[increase_mask] * eta_plus, torch.tensor(delta_max, device=parameter.device))         # device comunica su quale dispositivo (CPU o GPU) avviene il calolo
+                step_size[increase_mask] = torch.min(step_size[increase_mask] * eta_plus, torch.tensor(delta_max, device=parameter.device))
                 step_size[decrease_mask] = torch.max(step_size[decrease_mask] * eta_minus, torch.tensor(delta_min, device=parameter.device))
 
                 update = torch.zeros_like(parameter.data)
-                update[increase_mask | (grad_sign == 0)] = -torch.sign(grad[increase_mask | (grad_sign == 0)]) * step_size[increase_mask | (grad_sign == 0)]
-                update[decrease_mask] = -prev_update[decrease_mask]
-                grad[decrease_mask] = 0
+                update[increase_mask | (grad_sign == 0) | decrease_mask] = -torch.sign(grad[increase_mask | (grad_sign == 0) | decrease_mask]) * step_size[increase_mask | (grad_sign == 0) | decrease_mask]
 
                 parameter.data += update
 
